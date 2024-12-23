6776bfdf3c7fa82e7097432732ea7814099a59ba
JormuangHP <keyin@hehaibrand.com>
2024-12-23 09:18:20 +0800

    更新: 修复配置文件和构建设置

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,17 @@ module.exports = async function (env, argv) {
     config.output.publicPath = '/yingxiaorili/';
   }
 
+  // 添加字体文件支持
+  config.module.rules.push({
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: {
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'fonts/'
+      }
+    }
+  });
+
   return config;
 };
